f43b1837cceab720664ed4659d48eb2aef48e355
PhilippBusc <pbuschm3@uni-koeln.de>
2023-09-18 17:37:14 +0200

    New case: all observations are missing in one period (adjusted filter)

--- R/02_kf_MF.R
+++ R/02_kf_MF.R
@@ -61,6 +61,7 @@ kfMFPD <- function(yObs, uReg, wReg,
   BuReg     <- computeMatReg(mat = B, reg = uReg, dim = dimX, lenT = TT)
   DwReg     <- computeMatReg(mat = D, reg = wReg, dim = dimY, lenT = TT)
 
+
   xtt1 <- computeXtt1(A, x00, BuRegInit[, 1], dimX)
   Ptt1 <- computePtt1(A, P00, Q)
 
@@ -68,6 +69,8 @@ kfMFPD <- function(yObs, uReg, wReg,
     xtt1STORE[, 1]   <- xtt1
     Ptt1STORE[, , 1] <- Ptt1
   }
+
+  AllObsMissing <- FALSE
 
   for(t in 1:TT) {
     CAdj <- C
@@ -80,28 +83,39 @@ kfMFPD <- function(yObs, uReg, wReg,
     DwRegAdj <- DwReg[, t]
     MissingObs <- which(is.na(yObsAdj))
 
-    if(length(MissingObs) != 0){
-      W <- diag(dimY)
-      W <- W[-MissingObs, ]
+    if (length(MissingObs) != 0) {
+      if (length(MissingObs) == dimY) {
+        AllObsMissing <- TRUE
+      } else {
+        W <- diag(dimY)
+        W <- W[-MissingObs, ]
 
-      yObsAdj[MissingObs] <- 0
-      CAdj[MissingObs, ] <- 0
-      RAdj[MissingObs, ] <- 0
-      RAdj[ ,MissingObs] <- 0
+        yObsAdj[MissingObs] <- 0
+        CAdj[MissingObs, ] <- 0
+        RAdj[MissingObs, ] <- 0
+        RAdj[ ,MissingObs] <- 0
 
-      CAdj <- W %*% CAdj
-      RAdj <- W %*% RAdj %*% t(W)
-      yObsAdj <- W %*% yObsAdj
-      DwRegAdj <- W %*% DwRegAdj
+        CAdj <- W %*% CAdj
+        RAdj <- W %*% RAdj %*% t(W)
+        yObsAdj <- W %*% yObsAdj
+        DwRegAdj <- W %*% DwRegAdj
+      }
     }
 
-    Lt   <- computeLt(CAdj, Ptt1, RAdj)
-    Kt   <- computeKt(Ptt1, CAdj)
+    if(!AllObsMissing) {
+      Lt   <- computeLt(CAdj, Ptt1, RAdj)
+      Kt   <- computeKt(Ptt1, CAdj)
 
-    # period t quantities for current iteration
-    kGain      <- computekG(yObsAdj, CAdj, xtt1, DwRegAdj)
-    xtt[, t]   <- computeXtt(xtt1, Kt, Lt, kGain)
-    Ptt[, , t] <- computePtt(Ptt1, Kt, Lt, CAdj, RAdj, t)
+
+      # period t quantities for current iteration
+      kGain      <- computekG(yObsAdj, CAdj, xtt1, DwRegAdj)
+      xtt[, t]   <- computeXtt(xtt1, Kt, Lt, kGain)
+      Ptt[, , t] <- computePtt(Ptt1, Kt, Lt, CAdj, RAdj, t)
+    } else {
+      xtt[, t] <- xtt1
+      Ptt[, , t] <- Ptt1
+    }
+
 
     xtt1 <- computeXtt1(A, xtt[, t], BuReg[, t], dimX)
     Ptt1 <- computePtt1(A, Ptt[, , t], Q)
@@ -116,6 +130,7 @@ kfMFPD <- function(yObs, uReg, wReg,
   if (PDSTORE) {
     out <- list(mfdEXP = xtt, mfdVAR = Ptt,
                 pddEXP = xtt1STORE, pddVAR = Ptt1STORE)
+
   } else {
     out <- list(mfdEXP = xtt, mfdVAR = Ptt)
   }
